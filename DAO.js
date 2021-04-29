865f5c76536efc5e6fff36fa9f345986fffc1744
MartinLoera <jesusmartinloera@gmail.com>
2021-04-29 05:48:54 -0600

    Script.js terminado

--- DAO.js
+++ DAO.js
@@ -1,15 +1,39 @@
-const fs = require('fs');
-let rawdata = fs.readFileSync('items.json');
+const fs = require("fs");
+let rawdata = fs.readFileSync("items.json");
 
 class Inventory {
-    constructor() {
-        this.products = JSON.parse(rawdata);
-    }
+  constructor() {
+    this.products = JSON.parse(rawdata);
+  }
 
-    grater_stock(number_){
-        return this.products.filter(item => item.stock > number_)
-    }
-
+  grater_stock() {
+    return this.products.filter((item) => item.stock > 20);
+  }
+  less_stock() {
+    return this.products.filter((item) => item.stock < 15);
+  }
+  prices() {
+    return this.products.filter((item) => item.price > 20.3 && item.price < 45);
+  }
+  classification_price_less() {
+    return this.products.filter(
+      (item) => item.price > 20 && item.classification === "higiene"
+    );
+  }
+  same_class() {
+    let arr1 = this.products.filter(
+      (item) => item.classification === "verduras"
+    );
+    let arr2 = this.products.filter((item) => item.classification === "frutas");
+    let arr3 = this.products.filter(
+      (item) => item.classification === "aperitivos"
+    );
+    let arr4 = this.products.filter(
+      (item) => item.classification === "higiene"
+    );
+    let groups = [arr1, arr2, arr3, arr4];
+    return groups;
+  }
 }
 
 module.exports = Inventory;
--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-const Script = require('./DAO');
+const Script = require("./DAO");
 
 const products = new Script();
 
-console.log(products.grater_stock())
+let items = products.grater_stock();
+console.log("Número de productos con existencia mayor a 20:");
+console.log(items.length);
+items = products.less_stock();
+console.log("Número de productos con existencia menos a 15");
+console.log(items.length);
+items = products.prices();
+console.log(
+  "Lista de productos con la misma clasificación y precio mayor 15.50"
+);
+console.log(items);
+items = products.classification_price_less();
+console.log("Lista de productos con precio mayor a 20.30 y menor a 45.00");
+console.log(items);
+console.log("Número de productos agrupados por su clasificación");
+items = products.same_class();
+items.forEach((clas) => {
+  console.log(`\n${clas[0].classification}\n`);
+  console.log(clas.length);
+});
